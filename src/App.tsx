d877514371dec0369d46a074988401c3a13fd3cd
BTMLTECH <btmltechnology@gmail.com>
2025-09-12 07:40:10 +0100

    new bac1

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ import NotFound from "./pages/NotFound";
 import { BookingProvider } from "./hooks/BookingContext";
 import PaymentFailed from "./components/Failed";
 import PaymentSuccess from "./components/Success";
-import PaymentCallback from "./components/PaymentCallback";
-// import PaymentCallback from "./components/PaymentCallback";
 
 const queryClient = new QueryClient();
 
@@ -22,7 +20,6 @@ const App = () => (
        <BookingProvider>
         <Routes>
         <Route path="/" element={<Index />} />
-         <Route path="/payment/callback" element={<PaymentCallback />} /> 
         <Route path="/payment/success" element={<PaymentSuccess />} />
         <Route path="/payment/failed" element={<PaymentFailed />} />
           <Route path="*" element={<NotFound />} />
